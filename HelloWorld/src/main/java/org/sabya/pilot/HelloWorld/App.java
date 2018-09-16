520c851c97b50a6104f333f7f2dcbbc030456632
sabya <sabya.workspace@gmail.com>
2018-09-16 12:33:04 +0530

    ok

---
+++ HelloWorld/src/main/java/org/sabya/pilot/HelloWorld/App.java
@@ -0,0 +1,13 @@
+package org.sabya.pilot.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
